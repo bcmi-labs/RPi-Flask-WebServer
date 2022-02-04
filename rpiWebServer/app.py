11f5e650fe33229ef09f24fafe7741a9302f5bf0
Massimo Pennazio <maxipenna@libero.it>
2022-02-04 17:25:14 +0100

    Added Portenta-X8 customizations

--- rpiWebServer/app.py
+++ rpiWebServer/app.py
@@ -1,91 +1,93 @@
 '''
-	Raspberry Pi GPIO Status and Control
+    Portenta-X8 GPIO Status and Control
 '''
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
+import gpio as GPIO
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+#GPIO.setmode(GPIO.BOARD)
+#GPIO.setwarnings(False)
 
 #define sensors GPIOs
-button = 20
-senPIR = 16
+#@TODO: below fake gpios, fixme
+button1 = 12
+button2 = 13
 
 #define actuators GPIOs
-ledRed = 13
-ledYlw = 19
-ledGrn = 26
+gpio0 = 160
+gpio1 = 161
+gpio2 = 162
 
 #initialize GPIO status variables
-buttonSts = 0
-senPIRSts = 0
-ledRedSts = 0
-ledYlwSts = 0
-ledGrnSts = 0
+button1Sts = 0
+button2Sts = 0
+gpio0Sts = 0
+gpio1Sts = 0
+gpio2Sts = 0
 
-# Define button and PIR sensor pins as an input
-GPIO.setup(button, GPIO.IN)   
-GPIO.setup(senPIR, GPIO.IN)
+# Define button1 and button2 pins as an input
+GPIO.setup(button1, GPIO.IN)
+GPIO.setup(button2, GPIO.IN)
 
-# Define led pins as output
-GPIO.setup(ledRed, GPIO.OUT)   
-GPIO.setup(ledYlw, GPIO.OUT) 
-GPIO.setup(ledGrn, GPIO.OUT) 
+# Define gpio pins as output
+GPIO.setup(gpio0, GPIO.OUT)
+GPIO.setup(gpio1, GPIO.OUT)
+GPIO.setup(gpio2, GPIO.OUT)
 
-# turn leds OFF 
-GPIO.output(ledRed, GPIO.LOW)
-GPIO.output(ledYlw, GPIO.LOW)
-GPIO.output(ledGrn, GPIO.LOW)
-	
+# turn gpios OFF
+GPIO.output(gpio0, GPIO.LOW)
+GPIO.output(gpio1, GPIO.LOW)
+GPIO.output(gpio2, GPIO.LOW)
+
 @app.route("/")
 def index():
-	# Read GPIO Status
-	buttonSts = GPIO.input(button)
-	senPIRSts = GPIO.input(senPIR)
-	ledRedSts = GPIO.input(ledRed)
-	ledYlwSts = GPIO.input(ledYlw)
-	ledGrnSts = GPIO.input(ledGrn)
+    # Read GPIO Status
+    button1Sts = GPIO.input(button1)
+    button2Sts = GPIO.input(button2)
+    gpio0Sts = GPIO.input(gpio0)
+    gpio1Sts = GPIO.input(gpio1)
+    gpio2Sts = GPIO.input(gpio2)
 
-	templateData = {
-      'button'  : buttonSts,
-      'senPIR'  : senPIRSts,
-      'ledRed'  : ledRedSts,
-      'ledYlw'  : ledYlwSts,
-      'ledGrn'  : ledGrnSts,
-      }
-	return render_template('index.html', **templateData)
-	
+    templateData = {
+        'button1' : button1Sts,
+        'button2' : button2Sts,
+        'gpio0'   : gpio0Sts,
+        'gpio1'   : gpio1Sts,
+        'gpio2'   : gpio2Sts,
+    }
+    return render_template('index.html', **templateData)
+
 # The function below is executed when someone requests a URL with the actuator name and action in it:
 @app.route("/<deviceName>/<action>")
 def action(deviceName, action):
-	if deviceName == 'ledRed':
-		actuator = ledRed
-	if deviceName == 'ledYlw':
-		actuator = ledYlw
-	if deviceName == 'ledGrn':
-		actuator = ledGrn
-   
-	if action == "on":
-		GPIO.output(actuator, GPIO.HIGH)
-	if action == "off":
-		GPIO.output(actuator, GPIO.LOW)
-		     
-	buttonSts = GPIO.input(button)
-	senPIRSts = GPIO.input(senPIR)
-	ledRedSts = GPIO.input(ledRed)
-	ledYlwSts = GPIO.input(ledYlw)
-	ledGrnSts = GPIO.input(ledGrn)
-   
-	templateData = {
-	  'button'  : buttonSts,
-      'senPIR'  : senPIRSts,
-      'ledRed'  : ledRedSts,
-      'ledYlw'  : ledYlwSts,
-      'ledGrn'  : ledGrnSts,
-	}
-	return render_template('index.html', **templateData)
+    if deviceName == 'gpio0':
+        actuator = gpio0
+    if deviceName == 'gpio1':
+        actuator = gpio1
+    if deviceName == 'gpio2':
+        actuator = gpio2
+
+    if action == "on":
+        GPIO.output(actuator, GPIO.HIGH)
+    if action == "off":
+        GPIO.output(actuator, GPIO.LOW)
+
+    button1Sts = GPIO.input(button1)
+    button2Sts = GPIO.input(button2)
+    gpio0Sts = GPIO.input(gpio0)
+    gpio1Sts = GPIO.input(gpio1)
+    gpio2Sts = GPIO.input(gpio2)
+
+    templateData = {
+        'button1' : button1Sts,
+        'button2' : button2Sts,
+        'gpio0'   : gpio0Sts,
+        'gpio1'   : gpio1Sts,
+        'gpio2'   : gpio2Sts,
+    }
+    return render_template('index.html', **templateData)
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=True)
